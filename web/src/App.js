cb11569cc10c4e36c1e9ca5653d508b9bea5debb
shidomnt <hatranquang02@gmail.com>
2022-10-09 15:41:19 +0700

    add room

--- web/src/App.js
+++ web/src/App.js
@@ -6,19 +6,21 @@ import { Homepage } from './pages/Homepage'
 import { TokenAvailble, TokenUnavailble } from './auth/Auth'
 import { AppContextProvider } from './context/AppContext'
 import { Game } from './pages/Game'
+import Room from './pages/Room'
 
 function App() {
-
   return (
     <AppContextProvider>
       <Routes>
-        <Route element={<TokenAvailble/>}>
-          <Route path="/" element={<Login/>} />
-          <Route path="/login" element={<Login/>} />
+        <Route element={<TokenAvailble />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
         </Route>
-        <Route element={<TokenUnavailble/>}>
-          <Route path="/home-page" element={<Homepage/>} />
-          <Route path="/game" element={<Game/>} />
+        <Route element={<TokenUnavailble />}>
+          <Route path="/home-page" element={<Homepage />} />
+          <Route path=":roomId" element={<Room />}>
+            <Route path=":gameId" element={<Game />} />
+          </Route>
         </Route>
       </Routes>
     </AppContextProvider>
